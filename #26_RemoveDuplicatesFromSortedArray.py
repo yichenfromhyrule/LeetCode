1afc349b387727329b1a701052090460ec089639
yichenfromhyrule <jambako1995@gmail.com>
2021-06-19 10:52:22 -0400

    New Questions

--- #26_RemoveDuplicatesFromSortedArray.py
+++ #26_RemoveDuplicatesFromSortedArray.py
@@ -1,4 +1,5 @@
 class Solution(object):
+
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
---
+++ #27_RemoveElement.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if len(nums)==0:
+            return 0
+        elif len(nums)==1:
+            if nums[0]==val:
+                nums[0]='_'
+                return 0
+            else:
+                return 1
+        else:
+            if nums[0]==val:
+                nums[0]='_'
+            for i in range(1, len(nums)):
+                if nums[i]==val:
+                    nums[i]='_'
+                else:
+                    if nums[i-1]=='_':
+                        index_symbol = nums.index('_')
+                        nums[index_symbol]=nums[i]
+                        nums[i]='_'
+                #print(nums)
+            return len(nums)-nums.count('_')
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [0,1,2,2,3,0,4,2]
+    r = s.removeElement(nums,2)
+    print(r)
