7dcc615db5275d1fd07c5644a3de696f3f92f34a
yichenfromhyrule <jambako1995@gmail.com>
2021-06-27 09:59:49 -0400

    New Questions

--- #121_BestTimeToBuyAndSellStock.py
+++ #121_BestTimeToBuyAndSellStock.py
@@ -8,22 +8,24 @@ class Solution(object):
             return 0
         else:
             dp=[]
+            profit = prices[1] - prices[0]
+            dp.append(profit)
+            maxProfit = profit
+            left_index = 0
+            min_value = 0 if prices[0]<=prices[1] else 1
             if len(prices)==2:
-                profit = prices[1]-prices[0]
-                return profit if profit>0 else 0
+                return maxProfit if maxProfit>0 else 0
             else:
-                profit = prices[1] - prices[0]
-                dp.append(profit)
-                maxProfit = profit
-                maxProfitIndex = 0
                 for i in range(2, len(prices)):
-                    print("current maxIndex=%d, maxProfit=%d, prices[%d]-prices[%d]=%d, prices[%d]-prices[%d]=%d."%(maxProfitIndex,dp[len(dp)-1],i,i-1,prices[i]-prices[i-1],i, maxProfitIndex,prices[i]-prices[maxProfitIndex]))
-                    profit = max(dp[len(dp)-1],prices[i]-prices[i-1],prices[i]-prices[maxProfitIndex])
+                    min_value = i if prices[i]<prices[min_value] else min_value
+                    print("current left Index=%d, maxProfit at this point =%d, prices[%d]-prices[%d]=%d, prices[%d]-prices[%d]=%d. prices[%d]-min value %d= %d"\
+                          %(left_index,dp[len(dp)-1],i,i-1,prices[i]-prices[i-1],i, left_index,prices[i]-prices[left_index], i,min_value, prices[i]-prices[min_value]))
+                    profit = max(dp[len(dp)-1],prices[i]-prices[i-1],prices[i]-prices[min_value])
                     dp.append(profit)
                     if profit>=maxProfit:
                         maxProfit = profit
-                        if profit==prices[i]-prices[i-1]:
-                            maxProfitIndex = i-1 if prices[i-1]<prices[maxProfitIndex] else maxProfitIndex
+                        if profit==prices[i]-prices[min_value]:
+                            left_index = min_value if prices[min_value]<prices[left_index] else left_index
                     print(dp)
             return maxProfit if maxProfit>0 else 0
 
@@ -32,6 +34,6 @@ class Solution(object):
 
 if __name__ == '__main__':
     s = Solution()
-    nums = [2,7,1,4,11]
+    nums = [2,1,2,1,0,1,2]
     r = s.maxProfit(nums)
     print(r)
---
+++ #338_CountingBits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    n = 2
+    r = s.countBits(n)
+    print(r)
