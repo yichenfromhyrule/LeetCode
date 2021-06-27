ac2615b07ff2fbb0b950c904c07c66e79a5436a5
yichenfromhyrule <jambako1995@gmail.com>
2021-06-27 19:17:18 -0400

    New Questions

--- #338_CountingBits.py
+++ #338_CountingBits.py
@@ -1,9 +1,24 @@
+import math
 class Solution(object):
     def countBits(self, n):
         """
         :type n: int
         :rtype: List[int]
         """
+        result = [0, 1]
+        if n <= 1:
+            return result[:n + 1]
+        else:
+            sqrt_i = 0
+            for i in range(2, n + 1):
+                sqrt_i = math.log(i, 2)
+                if sqrt_i == int(sqrt_i):
+                    sqrt_i = int(sqrt_i)
+                    result.append(1)
+                else:
+                    sqrt_i = int(sqrt_i)
+                    result.append(1 + result[i - 2 ** sqrt_i])
+        return result
 
 
 if __name__ == '__main__':
