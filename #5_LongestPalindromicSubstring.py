ed5c89621dcff7263d75f5b90702539d9d3e19af
yichenfromhyrule <jambako1995@gmail.com>
2021-07-02 21:51:33 -0400

    New Questions

---
+++ #5_LongestPalindromicSubstring.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    str = "babab"
+    r = s.longestPalindrome(str)
+    print(r)
