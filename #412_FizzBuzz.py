dc55e14a0ae3fc369752f967bc20d06049e89dca
yichenfromhyrule <jambako1995@gmail.com>
2021-07-06 23:00:49 -0400

    New Questions

---
+++ #412_FizzBuzz.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for i in range(1,n+1):
+            if i % 15 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
+if __name__ == '__main__':
+    s = Solution()
+    n = 15
+    r= s.fizzBuzz(n)
+    print(r)
