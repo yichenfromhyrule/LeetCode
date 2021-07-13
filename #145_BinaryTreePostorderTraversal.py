265cfcf305596f687b22bab7fdba41c94e02df1d
yichenfromhyrule <jambako1995@gmail.com>
2021-07-12 23:20:11 -0400

    New Questions

---
+++ #145_BinaryTreePostorderTraversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        self.helperFunction(root, result)
+        return result
+
+    def helperFunction(self, root, result):
+        if root:
+            self.helperFunction(root.left, result)
+            self.helperFunction(root.right, result)
+            result.append(root.val)
