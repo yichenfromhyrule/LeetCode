462b89ef937af39313bb2a204025106a0e8dfa61
yichenfromhyrule <jambako1995@gmail.com>
2021-07-18 00:04:56 -0400

    New dataset

---
+++ #1935_MaximumNumberOfWordsYouCanType.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canBeTypedWords(self, text, brokenLetters):
+        """
+        :type text: str
+        :type brokenLetters: str
+        :rtype: int
+        """
+        hashmap = {}
+        for letter in brokenLetters:
+            hashmap[letter] = True
+        text_array = text.split(" ")
+        count = 0
+        for text_item in text_array:
+            broken = False
+            for char_item in text_item:
+                if char_item in hashmap:
+                    broken = True
+            count += 1 if not broken else 0
+        return count
