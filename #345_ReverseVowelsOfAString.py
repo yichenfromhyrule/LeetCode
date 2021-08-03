68b9469db98f4ee5ffb260e429b4f275b0c16bb1
yichenfromhyrule <jambako1995@gmail.com>
2021-08-03 08:45:44 -0400

    New Q

--- #345_ReverseVowelsOfAString.py
+++ #345_ReverseVowelsOfAString.py
@@ -1,20 +1,28 @@
 class Solution(object):
-    def reverseVowels(self, s):
+    def generateParenthesis(self, n):
         """
-        :type s: str
-        :rtype: str
+        :type n: int
+        :rtype: List[str]
         """
-        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        start = 0
-        end = len(s) - 1
-        while start < end:
-            if s[start] in vowels and s[end] in vowels:
-                s = s[:start] + s[end] + s[start+1:end] + s[start] + s[end+1:]
-                start += 1
-                end -= 1
-            else:
-                if s[start] not in vowels:
-                    start += 1
-                if s[end] not in vowels:
-                    end -= 1
-        return s
+        init_item = "(" * n + ")"
+        init_list = [init_item]
+        need_right = n - 1  # need n-1 ")"
+        while need_right > 0:
+            new_list = []
+            for curr_str in init_list:
+                check_left = n - need_right
+                i = 0
+                count_left = 0
+                while count_left < check_left:
+                    if curr_str[i] == "(":
+                        count_left += 1
+                    if count_left == check_left:
+                        new_str = curr_str[:i + 1] + ")" + curr_str[i + 1:]
+                        new_list.append(new_str)
+                        if check_left < n:
+                            check_left += 1
+                    i += 1
+            init_list = list(set(new_list))
+            need_right -= 1
+
+        return init_list
