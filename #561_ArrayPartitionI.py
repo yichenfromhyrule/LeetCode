050058c06bd1e8850f922d67fb58509243d73f5e
yichenfromhyrule <jambako1995@gmail.com>
2021-08-09 11:00:07 -0400

    New Q

---
+++ #561_ArrayPartitionI.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        result = 0
+        for i in range(0, len(nums)//2):
+            result += nums[i*2]
+        return result
